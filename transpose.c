576d7e947d2478e52277bcfb1f9cce37f74b8a53
justincong <justincong324@gmail.com>
2018-02-14 14:47:46 -0500

    proper modulo function added

--- transpose.c
+++ transpose.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// an improvement on C's % operator - enables proper negative modulo calculation
+int properMod(int a, int b) {
+
+    if (a < 0) {
+        a = a + b;
+    }
+
+    if (a < 0) {
+        properMod(a, b);
+    }
+    else {
+        return a % b;
+    }
+    return 0;
+
+}
+
 struct cipherArray {
     int length;
     int padding;
-    char *array; // pointer to the array
+    int array[];
 };
 
 struct cipherArray *
 cipherInitialize(int length, int padding){
+
+    struct cipherArray *input;
+
+    input = malloc(sizeof(struct cipherArray) + sizeof(int) * length);
+
+    input->length = length;
+    input->padding = padding;
+
 
 }
 
@@ -23,8 +48,19 @@ main(int argc, char **argv) {
     }
 
     // ensure n is at least 1
-    if (argv[1] > 0) {
+    if (argv[1] < (char *) 1) {
         fprintf(stderr, "Ensure n is at least 0\n");
     }
+
+    int c;
+    int counter = 0;
+
+    while ((c = getchar()) != EOF) {
+        counter++;
+    }
+
+    printf("%d\n", counter);
+
+    return 0;
 
 }
