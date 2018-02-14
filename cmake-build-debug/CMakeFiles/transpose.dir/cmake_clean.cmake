e2b23c1f2c6d2b78f279af59d4d9dcf5cc479203
justincong <justincong324@gmail.com>
2018-02-14 12:14:53 -0500

    initial commit

---
+++ cmake-build-debug/CMakeFiles/transpose.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/transpose.dir/main.c.o"
+  "transpose.pdb"
+  "transpose"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/transpose.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
