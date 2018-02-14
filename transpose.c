7c453573d3ba80e210c8f8a2f2bc073c3792e787
justincong <justincong324@gmail.com>
2018-02-14 13:19:08 -0500

    intial error-checking

--- transpose.c
+++ transpose.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv) {
+struct cipherArray {
+    int length;
+    int padding;
+    char *array; // pointer to the array
+};
 
-    if (argc != 1) {
-        fprintf(stderr, "Usage: %s\n", argv[0]);
+struct cipherArray *
+cipherInitialize(int length, int padding){
+
+}
+
+
+int
+main(int argc, char **argv) {
+
+    // ensure three and only three arguments are piped in
+    if (argc != 4) {
+        fprintf(stderr, "Usage: ./transpose n a b\n");
         return 1;
     }
+
+    // ensure n is at least 1
+    if (argv[1] > 0) {
+        fprintf(stderr, "Ensure n is at least 0\n");
+    }
+
 }
